a37411514b5a25296e8f49b5c5984f1fce9f2bc1
Travis CI User <travis@example.org>
2017-01-31 05:22:44 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["porteurbars"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='porteurbars/git/enum.Url.html' title='porteurbars::git::Url'>Url</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='porteurbars/enum.Error.html' title='porteurbars::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='porteurbars/struct.Template.html' title='porteurbars::Template'>Template</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ porteurbars/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Error","Enumeration of types of errors"]],"mod":[["git",""]],"struct":[["Template","A template holds a path to template source and a file describing the default values associated with names used in the template"]],"type":[["Result",""]]});
